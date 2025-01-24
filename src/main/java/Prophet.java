ca65b31dfa745ab2ea4d9d127bc4417ce6200e3e
tim0tay <timtay2002@gmail.com>
2025-01-24 11:09:00 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Prophet.java
@@ -0,0 +1,10 @@
+public class Prophet {
+    public static void main(String[] args) {
+        String horizontalLine = "--------------------------------------------------------------------------------\n";
+        System.out.println(horizontalLine
+                + "Hi! Welcome to the Daily Prophet! You can call me Prophet for short.\n"
+                + "What can I do for you?\n"
+                + horizontalLine);
+        System.out.println("Bye! Hope to see you again!\n" + horizontalLine);
+    }
+}
