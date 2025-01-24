483d979cb0f6bbe1a935ced28adea70e319c80d9
tim0tay <timtay2002@gmail.com>
2025-01-24 11:21:03 +0800

    Level-1: Echo

--- src/main/java/Prophet.java
+++ src/main/java/Prophet.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Prophet {
     public static void main(String[] args) {
         String horizontalLine = "--------------------------------------------------------------------------------\n";
@@ -5,6 +6,12 @@ public class Prophet {
                 + "Hi! Welcome to the Daily Prophet! You can call me Prophet for short.\n"
                 + "What can I do for you?\n"
                 + horizontalLine);
-        System.out.println("Bye! Hope to see you again!\n" + horizontalLine);
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        while (!str.equals("bye")) {
+            System.out.println(horizontalLine + str + "\n" + horizontalLine);
+            str = sc.next();
+        }
+        System.out.println(horizontalLine + "Bye! Hope to see you again!\n" + horizontalLine);
     }
 }
