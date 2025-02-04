450b3612767b5f08fb78a0c117a46d2cf4f12493
tim0tay <timtay2002@gmail.com>
2025-02-05 00:34:16 +0800

    A-JUnit

---
+++ src/test/java/prophet/todolist/TodoListTest.java
@@ -0,0 +1,26 @@
+package prophet.todolist;
+
+import org.junit.jupiter.api.Test;
+import prophet.task.ToDoTask;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoListTest {
+    @Test
+    public void testAddToListNull() {
+        assertEquals("Please.. don't make me work harder than I have to!\n",
+                new TodoList().addToList(null));
+    }
+    @Test
+    public void testAddToListTest() {
+        assertEquals("Task added to list: T | [ ] test\n",
+                new TodoList().addToList(new ToDoTask("test")));
+    }
+
+    @Test
+    public void testEnumerateListEmpty() {
+        assertEquals("Let's see what you have on your plate:\nSo gewd ah.. nothing to do!\n",
+                new TodoList().enumerateList());
+    }
+
+}
